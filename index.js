a019f7cdfbc6d16b33d8d4b98a5416dc31d38781
Ignacio Palma <naxopalma@gmail.com>
2017-03-13 16:42:51 -0300

    sendfile instead of render

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ var express = require('express');
 var mongoose = require('mongoose');
 
 var app = express();
+// app.set('view engine', 'html');
 
 app.get('/', function(request, response) {
-	response.render('pages/index')
+	response.sendfile('client/index.html')
 })
 
-app.listen(8000);
+app.listen(8000, function() {
+  console.log('Node app is running on port 8000');
+});
 
 module.exports = app;
