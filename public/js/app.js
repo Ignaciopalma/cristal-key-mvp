14fdc78eb197c89386e14a3d1bf2698a6848d9cd
Ignacio Palma <naxopalma@gmail.com>
2017-03-14 12:08:46 -0300

    db storing data, nobcrypt yet

--- public/js/app.js
+++ public/js/app.js
@@ -2,15 +2,57 @@ var app = angular.module('app', ["ngRoute"]);
 
 app.config(function($routeProvider) {
   $routeProvider
+  .when("/", {
+    templateUrl : "../../client/partials/main.html",   
+    controller: "mainController" 
+  })
   .when("/plan", {
-    templateUrl : "plan.html"
+    templateUrl : "../../client/partials/plan.html",    
+  })
+  .when("/signin", {
+    templateUrl : "../../client/partials/signin.html",    
+  })
+  .when("/signup", {
+    templateUrl : "../../client/partials/signup.html", 
+    controller: "signUpController"   
+  })
+  .when("/main", {
+    templateUrl : "../../client/partials/main.html",    
+    controller: "mainController" 
   })
 });
 
-app.controller("mainController", function($scope, passHash) {
+app.controller("mainController", function($scope, $location, passHash) {
+  $scope.destinations = ["facebook", "scotiabank", "twitter"];
 	$scope.getCristalPass = function() {
 		var secretWord = $scope.secretInput;
-		var destination = $scope.destination;
+    var destination = $scope.destination;
 		$scope.cristalpass = passHash.cristalHash(secretWord, destination);
+    $('.overlay').css('display', 'none');
 	}
+
+  $scope.passOverlay = function(destination) {
+    $('.overlay').css('display', 'block');
+    $scope.destination = destination;
+  }
+
+  $scope.newDestinataryOverlay = function() {
+    $('.new-destinatary-overlay').css('display', 'block');  
+  }
+
+  $scope.createNewDestinatary = function() {
+    $('.new-destinatary-overlay').css('display', 'none');  
+    var newDestinatary = $scope.newDestinatary;
+    $scope.destinations.push(newDestinatary);  
+  }
+});
+
+app.controller("signUpController", function($scope, $http) {  
+  $scope.createUser = function(username, password) {
+    $http({
+      method: 'POST',
+      url: '/users',
+      data: {username: username, password: password, destinations: []}
+    })
+  }
 })
--- server.js
+++ server.js
@@ -1,22 +1,68 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var path = require('path');
+var bcrypt = require('bcrypt-nodejs');
+var bodyParser = require('body-parser');
+
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost');
+
+var UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  password: {
+    type: String,
+    required: true
+  },
+
+  destinations: {
+  	type: Array
+  }
+});
+
+var UserData = mongoose.model('UserData', UserSchema);
 
 var app = express();
+app.use(bodyParser.json());
 app.set('port', (process.env.PORT || 5000));
-// app.set('view engine', 'html');
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(request, response) {
-	response.sendfile('client/index.html')
-})
-
-app.get('/plan', function(request, response) {
-	response.sendfile('client/plan.html')
-})
+app.use(express.static(path.join(__dirname, '/')));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port');
 });
+
+
+app.get('/', function(req, res) {
+	res.sendfile('client/index.html')
+});
+
+app.get('/users', function(req, res, next) {
+	console.log(res.body)
+	UserData.find().then(function(result){
+		console.log(result);
+	})
+})
+
+app.post('/users', function(req, res, next) {
+	console.log('post request comming in...')
+	console.log(req.body)
+
+	var user = {
+		username: req.body.username,
+		password: req.body.password,
+		destinations: req.body.destinations		
+	}
+
+	var data = new UserData(user);
+	data.save();
+})
+
+
 
 module.exports = app;
